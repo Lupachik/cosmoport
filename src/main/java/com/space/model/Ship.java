db6fe5d1516b0bc0b83a7a3fb973099693580368
Lupachik <lupachik@gamil.com>
2020-01-18 00:24:08 +0300

    1.add entyti Ship

---
+++ src/main/java/com/space/model/Ship.java
@@ -0,0 +1,40 @@
+package com.space.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "ship")
+public class Ship {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "planet")
+    private String planet;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "shipType")
+    private ShipType shipType;
+
+    @Column(name = "prodDate")
+    private Date prodDate;
+
+    @Column(name = "isUsed")
+    private Boolean isUsed;
+
+    @Column(name = "speed")
+    private Double speed;
+
+    @Column(name = "crewSize")
+    private Integer crewSize;ship
+
+    @Column(name = "rating")
+    private Double rating;
+
+}
