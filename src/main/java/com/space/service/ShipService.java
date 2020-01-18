42eff9419b2252b664edd55d1602f9da87fef80d
Lupachik <lupachik@gamil.com>
2020-01-18 20:06:32 +0300

    5.add work logics in service and ShipController

--- src/main/java/com/space/service/ShipService.java
+++ src/main/java/com/space/service/ShipService.java
@@ -1,6 +1,7 @@
 package com.space.service;
 
 import com.space.model.Ship;
+import com.space.model.ShipType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -15,4 +16,20 @@ public interface ShipService {
     Ship editShip(Long id, Ship ship);
     void deleteById(Long id);
     Long checkAndParseId(String id);
+
+    Specification<Ship> filterByPlanet(String planet);
+
+    Specification<Ship> filterByName(String name);
+
+    Specification<Ship> filterByShipType(ShipType shipType);
+
+    Specification<Ship> filterByDate(Long after, Long before);
+
+    Specification<Ship> filterByUsage(Boolean isUsed);
+
+    Specification<Ship> filterBySpeed(Double min, Double max);
+
+    Specification<Ship> filterByCrewSize(Integer min, Integer max);
+
+    Specification<Ship> filterByRating(Double min, Double max);
 }
